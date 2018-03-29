c4daf3f63c1bacf8e7efc96f7c237d2972023027
aikin <1@aikin.me>
2018-03-29 13:31:07 +0800

    aikin: extract api base test

---
+++ src/test/java/me/aikin/mockbean/ApiBaseTestSetup.java
@@ -0,0 +1,34 @@
+package me.aikin.mockbean;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import com.jayway.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class ApiBaseTestSetup {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+
+    public MockMvcRequestSpecification given() {
+        return RestAssuredMockMvc
+                .given()
+                .header("Accept", ContentType.JSON.withCharset("UTF-8"))
+                .header("Content-Type", ContentType.JSON.withCharset("UTF-8"));
+    }
+}
